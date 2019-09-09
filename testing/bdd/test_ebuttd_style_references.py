7520addac66600a6da4b66f59e58bf013db1881c
Dan Pope <Dan.Pope@bbc.co.uk>
2019-09-09 16:33:48 +0100

    Remove duplication in function names, break xquery onto multiple lines...
    -  and check that only one style/region element exists before checking attributes

--- testing/bdd/test_ebuttd_style_references.py
+++ testing/bdd/test_ebuttd_style_references.py
@@ -8,7 +8,7 @@ scenarios('features/styles/ebuttd_style_references.feature')
 def when_it_contains_style(test_context, template_dict, style_name):
     if 'styles' not in template_dict:
         template_dict['styles'] = list()
-    style = {"id": style_name}    
+    style = {"id": style_name}
     template_dict['styles'].append(style)
     test_context[style_name] = style
 
@@ -19,12 +19,12 @@ def when_style_has_attribute(test_context, style_name, attribute, value):
 
 
 @when(parsers.parse('it contains some text with style "{style_name}"'))
-def when_style_has_attribute(template_dict, style_name):
+def when_text_has_style(template_dict, style_name):
     template_dict['text_style'] = style_name
 
 
 @when(parsers.parse('it contains some text with region "{region_name}"'))
-def when_style_has_attribute(template_dict, region_name):
+def when_text_has_region(template_dict, region_name):
     template_dict['text_region'] = region_name
 
 
@@ -32,7 +32,7 @@ def when_style_has_attribute(template_dict, region_name):
 def when_it_contains_region(test_context, template_dict, region_id):
     if 'regions' not in template_dict:
         template_dict['regions'] = list()
-    region = {"id": region_id}    
+    region = {"id": region_id}
     template_dict['regions'].append(region)
     test_context[region_id] = region
 
@@ -46,7 +46,9 @@ def when_region_has_attribute(test_context, region_id, attribute, value):
 def then_converted_document_has_style(test_context, style_name, attribute, value):
     ebuttd_document = test_context['ebuttd_document']
     tree = ET.fromstring(ebuttd_document.get_xml())
-    elements = tree.findall('{http://www.w3.org/ns/ttml}head/{http://www.w3.org/ns/ttml}styling/{http://www.w3.org/ns/ttml}style[@{http://www.w3.org/XML/1998/namespace}id="%s"]' %style_name)
+    elements = tree.findall('{http://www.w3.org/ns/ttml}head/'
+                            '{http://www.w3.org/ns/ttml}styling/'
+                            '{http://www.w3.org/ns/ttml}style[@{http://www.w3.org/XML/1998/namespace}id="%s"]' % style_name)
     assert len(elements) == 1
     assert elements[0].get('{http://www.w3.org/ns/ttml#styling}%s' % attribute) == value
 
@@ -55,13 +57,19 @@ def then_converted_document_has_style(test_context, style_name, attribute, value
 def then_converted_document_has_region_with_styling_attribute(test_context, region_id, attribute, value):
     ebuttd_document = test_context['ebuttd_document']
     tree = ET.fromstring(ebuttd_document.get_xml())
-    element = tree.find('{http://www.w3.org/ns/ttml}head/{http://www.w3.org/ns/ttml}layout/{http://www.w3.org/ns/ttml}region[@{http://www.w3.org/XML/1998/namespace}id="%s"]' %region_id)
-    assert element.get('{http://www.w3.org/ns/ttml#styling}%s' % attribute) == value
+    elements = tree.findall('{http://www.w3.org/ns/ttml}head/'
+                            '{http://www.w3.org/ns/ttml}layout/'
+                            '{http://www.w3.org/ns/ttml}region[@{http://www.w3.org/XML/1998/namespace}id="%s"]' % region_id)
+    assert len(elements) == 1
+    assert elements[0].get('{http://www.w3.org/ns/ttml#styling}%s' % attribute) == value
 
 
 @then(parsers.parse('the ebu_tt_d document contains style "{style_id}" without a "{attribute}" attribute'))
 def then_converted_document_has_style_without_attribute(test_context, style_id, attribute):
     ebuttd_document = test_context['ebuttd_document']
     tree = ET.fromstring(ebuttd_document.get_xml())
-    element = tree.find('{http://www.w3.org/ns/ttml}head/{http://www.w3.org/ns/ttml}styling/{http://www.w3.org/ns/ttml}style[@{http://www.w3.org/XML/1998/namespace}id="%s"]' % style_id)
-    assert element.get('{http://www.w3.org/ns/ttml#styling}%s' % attribute) == None
+    elements = tree.findall('{http://www.w3.org/ns/ttml}head/'
+                            '{http://www.w3.org/ns/ttml}styling/'
+                            '{http://www.w3.org/ns/ttml}style[@{http://www.w3.org/XML/1998/namespace}id="%s"]' % style_id)
+    assert len(elements) == 1
+    assert elements[0].get('{http://www.w3.org/ns/ttml#styling}%s' % attribute) == None

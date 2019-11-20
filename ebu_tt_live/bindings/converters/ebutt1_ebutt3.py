e68b8dee3714ed4523fff7c94c324ab90a0d91fc
nigelmegitt <nigel.megitt@bbc.co.uk>
2019-11-20 14:23:20 +0000

    Add EBU-TT 1 to EBU-TT 3 conversion
    Does not handle time conversion.
    Sets `ebuttp:sequenceIdentifier` to the value of `tt/head/metadata/documentMetadata/documentIdentifier` if present, otherwise uses "TestConverter".
    Resets the `conformsToStandard` to say it is EBU-TT-3 conformant.
    Sets the `timeBase` to `media` whether you like it or not, but doesn't do any other conversions.

--- ebu_tt_live/bindings/converters/ebutt1_ebutt3.py
+++ ebu_tt_live/bindings/converters/ebutt1_ebutt3.py
@@ -11,6 +11,7 @@ from pyxb.binding.basis import NonElementContent, ElementContent
 from datetime import timedelta
 import copy
 import logging
+from pyxb import ValidationError
 
 log = logging.getLogger(__name__)
 

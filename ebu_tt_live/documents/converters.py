32dc82eff6cd374b032cf8e87302fae1ff544728
nigelmegitt <nigel.megitt@bbc.co.uk>
2019-11-26 15:42:40 +0000

    Make a smpte timecode converter
    Make a FixedOffsetSMPTEtoTimedeltaConverter to convert SMPTE times based on the `ebuttm:documentStartOfProgramme` element if present, or assuming `00:00:00:00` otherwise.

--- ebu_tt_live/documents/converters.py
+++ ebu_tt_live/documents/converters.py
@@ -1,13 +1,12 @@
 
 from ebu_tt_live.bindings.converters.ebutt3_ebuttd import EBUTT3EBUTTDConverter
 from ebu_tt_live.bindings.converters.ebutt1_ebutt3 import EBUTT1EBUTT3Converter
+from ebu_tt_live.bindings.converters.timedelta_converter import \
+    FixedOffsetSMPTEtoTimedeltaConverter
 
 from ebu_tt_live.documents.ebuttd import EBUTTDDocument
 from ebu_tt_live.documents.ebutt1 import EBUTT1Document
 from ebu_tt_live.documents.ebutt3 import EBUTT3Document
-from subprocess import Popen, PIPE
-import tempfile
-import os
 import logging
 
 
@@ -16,7 +15,8 @@ log = logging.getLogger(__name__)
 
 def ebutt3_to_ebuttd(ebutt3_in, media_clock):
     """
-    This function takes an EBUTT3Document instance and returns the same document as an EBUTTDDocument instance.
+    This function takes an EBUTT3Document instance and returns the same
+    document as an EBUTTDDocument instance.
     :param ebutt3_in:
     :return:
     """
@@ -37,15 +37,37 @@ def ebutt3_to_ebuttd(ebutt3_in, media_clock):
 
 def ebutt1_to_ebutt3(ebutt1_in, sequence_id, use_doc_id_as_seq_id):
     """
-    This function takes an EBUTT1Document instance and returns the same document as an EBUTT3Document instance.
+    This function takes an EBUTT1Document instance and returns the same 
+    document as an EBUTT3Document instance.
     :param ebutt1_in:
     :return:
     """
-    converter = EBUTT1EBUTT3Converter(sequence_id=sequence_id, 
+    converter = EBUTT1EBUTT3Converter(
+        sequence_id=sequence_id, 
         use_doc_id_as_seq_id=use_doc_id_as_seq_id)
     doc_xml = ebutt1_in.get_xml()
     ebutt1_doc = EBUTT1Document.create_from_xml(doc_xml)
-    ebutt3_bindings = converter.convert_document(ebutt1_doc.binding)
+
+    smpte_converter = None
+    if ebutt1_doc.binding.timeBase == 'smpte':
+        start_of_programme = '00:00:00:00'
+        head_metadata = ebutt1_doc.binding.head.metadata
+        if head_metadata:
+            doc_metadata = head_metadata.documentMetadata
+            if doc_metadata and doc_metadata.documentStartOfProgramme:
+                start_of_programme = doc_metadata.documentStartOfProgramme
+        smpte_converter = \
+            FixedOffsetSMPTEtoTimedeltaConverter(
+                start_of_programme,
+                ebutt1_doc.binding.frameRate,
+                ebutt1_doc.binding.frameRateMultiplier,
+                ebutt1_doc.binding.dropMode
+            )
+
+    ebutt3_bindings = converter.convert_document(
+        ebutt1_doc.binding,
+        dataset=None,
+        smpte_to_timedelta_converter=smpte_converter)
     ebutt3_document = EBUTT3Document.create_from_raw_binding(ebutt3_bindings)
     ebutt3_document.validate()
     return ebutt3_document
--- ebu_tt_live/node/ebutt1_ebutt3_producer.py
+++ ebu_tt_live/node/ebutt1_ebutt3_producer.py
@@ -2,6 +2,8 @@ from ebu_tt_live.bindings.converters.ebutt1_ebutt3 import EBUTT1EBUTT3Converter
 from ebu_tt_live.documents.ebutt1 import EBUTT1Document
 from .base import AbstractCombinedNode
 from ebu_tt_live.documents import EBUTT3Document
+from ebu_tt_live.bindings.converters.timedelta_converter \
+    import FixedOffsetSMPTEtoTimedeltaConverter
 
 
 class EBUTT1EBUTT3ProducerNode(AbstractCombinedNode):
@@ -10,7 +12,9 @@ class EBUTT1EBUTT3ProducerNode(AbstractCombinedNode):
     _expects = EBUTT1Document
     _provides = EBUTT3Document
 
-    def __init__(self, node_id, consumer_carriage=None,
+    def __init__(self,
+                 node_id,
+                 consumer_carriage=None,
                  producer_carriage=None,
                  sequence_identifier=None,
                  use_document_identifier_as_sequence_identifier=True,
@@ -28,7 +32,27 @@ class EBUTT1EBUTT3ProducerNode(AbstractCombinedNode):
     def process_document(self, document, **kwargs):
         # Convert each receiver document into EBU-TT-3
         if self.is_document(document):
+            smpte_converter = None
+            if document.binding.tt.timeBase == 'smpte':
+                start_of_programme = '00:00:00:00'
+                head_metadata = document.binding.head.metadata
+                if head_metadata:
+                    doc_metadata = head_metadata.documentMetadata
+                    if doc_metadata and doc_metadata.documentStartOfProgramme:
+                        start_of_programme = \
+                            doc_metadata.documentStartOfProgramme
+                smpte_converter = \
+                    FixedOffsetSMPTEtoTimedeltaConverter(
+                        smpteReference=start_of_programme,
+                        frameRate=document.binding.tt.frameRate,
+                        frameRateMultiplier=document.binding.tt.
+                        frameRateMultiplier,
+                        dropMode=document.binding.tt.dropMode
+                    )
             converted_doc = EBUTT3Document.create_from_raw_binding(
-                self._ebutt3_converter.convert_document(document.binding)
+                self._ebutt3_converter.convert_document(
+                    document.binding,
+                    dataset=None,
+                    smpte_to_timedelta_converter=smpte_converter)
             )
             self.producer_carriage.emit_data(data=converted_doc, **kwargs)

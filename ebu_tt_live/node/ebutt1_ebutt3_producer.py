e7f8b8c65536d9d27b417c1398b333efdcfe4b11
nigelmegitt <nigel.megitt@bbc.co.uk>
2019-11-28 11:59:47 +0000

    Add documentation and fix node code

--- ebu_tt_live/node/ebutt1_ebutt3_producer.py
+++ ebu_tt_live/node/ebutt1_ebutt3_producer.py
@@ -7,7 +7,22 @@ from ebu_tt_live.bindings.converters.timedelta_converter \
 
 
 class EBUTT1EBUTT3ProducerNode(AbstractCombinedNode):
+    """
+    Produce an EBU-TT-3 document from an EBU-TT-1 input document.
 
+    For conversion of SMPTE timecodes, where `ttp:timeBase="smpte"`,
+    makes a 
+    :py:class:`ebu_tt_live.bindings.converters.timedelta_converter.FixedOffsetSMPTEtoTimedeltaConverter` using `00:00:00:00`
+    as the start of programme zero point, or if available, uses the
+    `ebuttm:documentStartOfProgramme` metadata value as the zero point,
+    and discards everything before the value of that element.
+
+    The output sequence identifier can be specified. Alternatively,
+    if the `use_document_identifier_as_sequence_identifier` parameter
+    is `True` and the `ebuttm:documentIdentifier` element is present
+    then that element's value is
+    used as the output sequence identifier.
+    """
     _ebutt3_converter = None
     _expects = EBUTT1Document
     _provides = EBUTT3Document
@@ -33,7 +48,7 @@ class EBUTT1EBUTT3ProducerNode(AbstractCombinedNode):
         # Convert each receiver document into EBU-TT-3
         if self.is_document(document):
             smpte_converter = None
-            if document.binding.tt.timeBase == 'smpte':
+            if document.binding.timeBase == 'smpte':
                 start_of_programme = '00:00:00:00'
                 head_metadata = document.binding.head.metadata
                 if head_metadata:
@@ -44,10 +59,10 @@ class EBUTT1EBUTT3ProducerNode(AbstractCombinedNode):
                 smpte_converter = \
                     FixedOffsetSMPTEtoTimedeltaConverter(
                         smpteReference=start_of_programme,
-                        frameRate=document.binding.tt.frameRate,
-                        frameRateMultiplier=document.binding.tt.
+                        frameRate=document.binding.frameRate,
+                        frameRateMultiplier=document.binding.
                         frameRateMultiplier,
-                        dropMode=document.binding.tt.dropMode
+                        dropMode=document.binding.dropMode
                     )
             converted_doc = EBUTT3Document.create_from_raw_binding(
                 self._ebutt3_converter.convert_document(

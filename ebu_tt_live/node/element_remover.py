34fadbf8d56bb20da2176744c489c0744549801b
nigelmegitt <nigel.megitt@bbc.co.uk>
2019-11-29 14:43:42 +0000

    Tidy and add/fix documentation

--- ebu_tt_live/node/element_remover.py
+++ ebu_tt_live/node/element_remover.py
@@ -1,12 +1,7 @@
 from .base import AbstractCombinedNode
 from ebu_tt_live.documents.ebutt3 import EBUTT3Document
-from pyxb.binding.basis import NonElementContent, ElementContent, \
+from pyxb.binding.basis import ElementContent, \
     complexTypeDefinition, simpleTypeDefinition
-from ebu_tt_live.bindings import div_type, p_type, span_type, ebuttdt, style_type
-from ebu_tt_live.bindings._ebuttm import divMetadata_type
-import copy
-import re
-from datetime import timedelta
 
 
 class ElementRemoverNode(AbstractCombinedNode):
@@ -15,7 +10,7 @@ class ElementRemoverNode(AbstractCombinedNode):
 
     Matches elements by local names only. Restricted to
     XML elements that have bindings; non-bound elements
-    are left in place. 
+    are left in place.
     """
 
     _expects = EBUTT3Document
@@ -38,7 +33,6 @@ class ElementRemoverNode(AbstractCombinedNode):
         )
 
         self.remove(remove_list)
-        print('remove_list = {}'.format(self._remove_list))
         self._sequence_identifier = sequence_identifier
         self._last_sequence_number = 0
 
@@ -46,7 +40,10 @@ class ElementRemoverNode(AbstractCombinedNode):
         """
         Specify the list of element local names to remove.
 
-        :param remove_list: Comma separated list of element names (can have white space)
+        The list string must be comma separated and can include
+        whitespace.
+
+        :param remove_list: element names to remove.
         """
         self._remove_list = []
         for r in remove_list.split(','):
@@ -55,9 +52,6 @@ class ElementRemoverNode(AbstractCombinedNode):
     def process_document(self, document, **kwargs):
         if self.is_document(document):
             if self.check_if_document_seen(document=document) is True:
-
-                print('Input document is:')
-                print(document.get_xml())
                 # Remove the elements we don't want
                 self.remove_unwanted_elements(document.binding)
 
@@ -66,8 +60,6 @@ class ElementRemoverNode(AbstractCombinedNode):
                 self._last_sequence_number += 1
                 document.sequence_identifier = self._sequence_identifier
                 document.sequence_number = self._last_sequence_number
-                print('Output document is:')
-                print(document.get_xml())
                 self.producer_carriage.emit_data(
                     data=document,
                     **kwargs
@@ -81,45 +73,25 @@ class ElementRemoverNode(AbstractCombinedNode):
 
     def remove_unwanted_elements(self, element):
         """Recursive function."""
-        import pdb
-
         children = []
-        try:
-            if isinstance(element, list):
-                print('this item is a list')
-            elif isinstance(element, complexTypeDefinition) and \
-                not element._IsSimpleTypeContent():
-                children = element.orderedContent()
-            elif isinstance(element, simpleTypeDefinition):
-                print('this item is too simple to iterate through')
-            elif element._IsSimpleTypeContent():
-                print('this item looks complex but it is only pretending')
-            else:
-                #pdb.set_trace()
-                print('this element is not a complex type, now what?')
-        except Exception as e:
-            print('got an exception type {}'.format(type(e)))
-            pdb.set_trace()
-            print('exception making orderedContent()')
+
+        # We only get a meaningful list of child elements from
+        # element.orderedContent() in some specific conditions!
+        if not isinstance(element, list) and \
+           not isinstance(element, simpleTypeDefinition) and \
+           isinstance(element, complexTypeDefinition) and \
+           not element._IsSimpleTypeContent():
+            children = element.orderedContent()
 
         for item in children:
-            if isinstance(item, NonElementContent):
-                print('this is not element content')
-                #pdb.set_trace()
-            elif isinstance(item, ElementContent):
+            # Some children might be NonElementContent, so check they are
+            # all genuine ElementContent before testing them further.
+            if isinstance(item, ElementContent):
                 if item.elementDeclaration is not None:
-                    #pdb.set_trace()
                     local_name = item.elementDeclaration.name().localName()
-                    namespace_uri = item.elementDeclaration.name().namespaceURI()
-                    fully_qualified_name = namespace_uri + '%%' + local_name
-                    print('Found an element with fully qualified name {}'.format(fully_qualified_name))
                     if (local_name in self._remove_list):
-                        print('I want to remove a {}'.format(local_name))
+                        # Remove this element
                         setattr(element, local_name, None)
                     else:
+                        # Recurse to the children
                         self.remove_unwanted_elements(item.value)
-            else:
-                print('this is neither NonElementContent nor ElementContent')
-                pdb.set_trace()
-                raise Exception('Can this even happen!??!?!?!')
-

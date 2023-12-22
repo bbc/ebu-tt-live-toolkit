f836baacfef50ad0f25e07ea4166f32dad3f3da5
Nigel Megitt <nigel.megitt@bbc.co.uk>
2023-12-22 17:37:39 +0000

    Expose description, name and version
    pyproject.toml doesn't import values from external files easily. Instead, specify description, namd and version in pyproject.toml and fetch them in project.py so that they can be exposed to other scripts, including when building the documentation.

--- ebu_tt_live/project.py
+++ ebu_tt_live/project.py
@@ -1,3 +1,17 @@
-version = '3.0.1'
-name = 'ebu-tt-live'
-description = 'EBU-TT Part 3 library implementing Specification EBU-3370'
+"""
+Expose project variables into python code and documentation
+"""
+import tomllib
+import os
+import pathlib
+
+# When building documentation, don't assume we're in the root folder
+path = os.path.dirname(__file__)
+ebu_tt_live_dir = os.path.dirname(path)
+project_toml_path = os.path.join(ebu_tt_live_dir, "pyproject.toml")
+with open(project_toml_path, "rb") as f:
+    _META = tomllib.load(f)
+
+name = _META["tool"]["poetry"]["name"]
+version = _META["tool"]["poetry"]["version"]
+description = _META["tool"]["poetry"]["description"]

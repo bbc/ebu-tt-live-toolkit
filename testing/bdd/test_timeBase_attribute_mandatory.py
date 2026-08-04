c42fcc3754af83c68b178f7d07b89c6ebdea61a5
Andrew Wheat <andrew.wheat@bbc.co.uk>
2026-08-04 11:58:18 +0100

    wip - fix timeBase_attribute_mandatory.feature

--- testing/bdd/test_timeBase_attribute_mandatory.py
+++ testing/bdd/test_timeBase_attribute_mandatory.py
@@ -1,9 +1,9 @@
 from pytest_bdd import when, scenarios
-
+from testing.bdd.conftest import legacy_name
 
 scenarios('features/validation/timeBase_attribute_mandatory.feature')
 
 
-@when('it has ttp:timeBase attribute <time_base>')
+@when(**legacy_name('it has ttp:timeBase attribute <time_base>'))
 def when_has_time_base(time_base, template_dict):
     template_dict['time_base'] = time_base
